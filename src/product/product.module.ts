db11e5b6431605b7d24374eb57d1136a5e2fffc5
Saista Chhipa <saista.chippa@creolestudios.com>
2022-04-11 17:16:39 +0530

    Initial Changes

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,10 +4,9 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './product.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   providers: [ProductService],
   controllers: [ProductController],
 })
