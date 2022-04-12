3e352a2e994ba49f16288bc1af278746404ea1f3
janviramani00 <ramanijanvi002gmail.com>
2022-04-12 18:33:09 +0530

    add product table

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ const entities = [ProductEntity, User, LogInUsers];
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
