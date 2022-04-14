c72a87f576c84482c67935f1184ee9d61f79755d
janviramani00 <ramanijanvi002gmail.com>
2022-04-14 10:51:14 +0530

    userservices/ return message m is changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ const entities = [ProductEntity, User, CartEntity, OrderEntity];
       type: process.env.DB_TYPE as any,
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: entities,
